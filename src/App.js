ac8646b741bc88dd7500a567b4898bf199da6aab
Eyyan-Amir <ayyanamir6@gmail.com>
2021-12-28 23:31:08 +0500

    new update

--- src/App.js
+++ src/App.js
@@ -1,102 +1,117 @@
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
+	const [input, setInput] = useState({});
+	const [bmi, setBmi] = useState(0);
 
-  const [input , setInput] = useState({});
-  const [bmi , setBmi] = useState("0");
+	const handleChange = (key, value) => {
+		setInput({
+			...input,
+			[key]: value,
+		});
+	};
 
-  const handleChange =(key,value)=>{
-    setInput({
-      ...input,
-      [key]:value
-    });
-  }
-  
-  const calculation =()=>{
-  
-      let weight = document.getElementById("weight").value;
-      let height = document.getElementById("height").value;
-      
-      height = height * 12;
-      height = height * 0.25;
-      
-      let meterSquare = height * height;
-      
-      let result = Math.round(weight / meterSquare * 100);
-      document.getElementById("BmiValue").value = result;
-      setBmi(result);
-      setInput("")
-    
-  }
-  const handleSubmit =(e)=>{
-    e.preventDefault();
-  }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let weight = input.weight;
+		let height = input.height;
 
-  return (
-    <div class="container">
-   <div class="card  m-40px style =  margin-top : 17%">
-  <div class="card-header text-center">BMI Calculator</div>
-  <div class="card-body">
-    <form class ="w-50 m-auto" onSubmit={handleSubmit} >
-      <div class ="form-group">
-        <label>Weight :</label>
-        <input type = "number"
-         id ="weight"
-         placeholder = "Wight in Kg"
-         min = "0"
-         max ="100"
-         maxLength = "4"
-         value={input.weight || ''}
-         onChange ={e => handleChange("weight",e.target.value)} 
-         class = "form-control"/>
-      </div>
-      <div class ="form-group">
-        <label>Height :</label>
-        <input type = "number"
-        id ="height"
-         placeholder = "height in ft"
-         value={input.height || ''}
-         onChange ={e => handleChange("height",e.target.value)} 
-         class ="form-control"/>
-      </div>
-      <div class ="form-group">
-        <label>BMI Value :</label>
-        <textarea type = "number"
-         id = "BmiValue"
-         onChange ={e => handleChange("BmiValue",e.target.value)}  
-         placeholder = "Result"
-         class ="form-control bmiValueControl"/>
-        
-        {bmi == 0 &&<><div>
-          </div></>
-          }
-        {bmi < 18.5 && bmi > 0 &&<><div
-        style = {{color : "Orange" , textShadow : "1px 1px black" , fontSize : "24px" , fontWeight : "bolder"}}>
-          you are under Weight
-          </div></>
-          }
+		height = height * 12;
+		height = height * 0.25;
 
-          {bmi > 18.5 && bmi < 25 &&<><div
-          style = {{color : "rgb(49, 194, 49)" , textShadow : "1px 1px black" , fontSize : "24px" , fontWeight : "bolder"}}>
-          you are Healthy
-          </div></>
-          }
-          {bmi > 25 &&<><div 
-          style = {{color : "Red" , textShadow : "1px 1px black" , fontSize : "24px" , fontWeight : "bolder"}}>
-          you are over Weight
-          </div></>}
-      </div>
-      <div>
-        <button type ="submit" onClick={calculation} class ="btn btn-success">BMI Value</button>
-      </div>
-    </form>
-  </div>
-  <div class="card-footer text-center">A Healthy BMI ranges between 19 to 25</div>
-</div>
-    
-    </div>
-  );
+		let meterSquare = height * height;
+
+		let result = Math.round((weight / meterSquare) * 100);
+		setBmi(result);
+		setInput("");
+	};
+
+	return (
+		<div className='container'>
+			<div className='card  m-40px style =  margin-top : 17%'>
+				<div className='card-header text-center'>BMI Calculator</div>
+				<div className='card-body'>
+					<form className='w-50 m-auto' onSubmit={handleSubmit}>
+						<div className='form-group'>
+							<label>Weight :</label>
+							<input
+								type='number'
+								id='weight'
+								placeholder='Wight in Kg'
+								value={input.weight || ""}
+								onChange={(e) => handleChange("weight", e.target.value)}
+								className='form-control'
+							/>
+						</div>
+						<div className='form-group'>
+							<label>Height :</label>
+							<input
+								type='number'
+								id='height'
+								placeholder='height in ft'
+								value={input.height || ""}
+								onChange={(e) => handleChange("height", e.target.value)}
+								className='form-control'
+							/>
+						</div>
+						<div className='form-group'>
+							<label>BMI Value :</label>
+							<textarea
+								type='number'
+								value={bmi}
+								id='BmiValue'
+								placeholder='Result'
+								className='form-control bmiValueControl'
+								readOnly
+							/>
+
+							{bmi == 0 && (
+								<>
+									<div></div>
+								</>
+							)}
+							{bmi < 18.5 && bmi > 0 && (
+								<>
+									<div style={{ color: "Orange", textShadow: "1px 1px black", fontSize: "24px", fontWeight: "bolder" }}>
+										you are under Weight
+									</div>
+								</>
+							)}
+
+							{bmi > 18.5 && bmi < 25 && (
+								<>
+									<div style={{ color: "rgb(49, 194, 49)", textShadow: "1px 1px black", fontSize: "24px", fontWeight: "bolder" }}>
+										you are Healthy
+									</div>
+								</>
+							)}
+							{bmi > 25 && (
+								<>
+									<div style={{ color: "Red", textShadow: "1px 1px black", fontSize: "24px", fontWeight: "bolder" }}>
+										you are over Weight
+									</div>
+								</>
+							)}
+						</div>
+						<div>
+							<button className='btn btn-success'>BMI Value</button>
+						</div>
+					</form>
+				</div>
+				<div className='card-footer text-center'>
+					A Healthy BMI ranges between{" "}
+					<span className='spanClas' style={{ color: "yellow" }}>
+						19
+					</span>{" "}
+					to{" "}
+					<span className='spanClas' style={{ color: "yellow" }}>
+						25
+					</span>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
